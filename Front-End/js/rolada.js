1e115190e45b484148291234aee8af94005f3f81
Ricardo <ricardoervilha456@gmail.com>
2022-11-27 11:52:30 -0300

    Gerenciamento de Users e Jogos v1.0

--- Front-End/js/rolada.js
+++ Front-End/js/rolada.js
@@ -1,6 +1,8 @@
 const button = document.querySelector(".seta")
+var caption = document.querySelector(".carousel-caption")
 
 button.addEventListener('click', () => {
     window.scroll({top: window.innerHeight, behavior: "smooth"})
     button.style.display = 'none'
+    caption.style.padding = "100px"
 })
