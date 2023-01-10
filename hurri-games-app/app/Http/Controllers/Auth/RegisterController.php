1edf4d34f6c3345e08ff2fbe0fe19963ad9388b5
Lucas Santana <lucasg4x@gmail.com>
2023-01-10 00:20:13 -0300

    Enviar e-mail de verificação ao registrar

--- hurri-games-app/app/Http/Controllers/Auth/RegisterController.php
+++ hurri-games-app/app/Http/Controllers/Auth/RegisterController.php
@@ -65,11 +65,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'birth_date' => $data['birth_date'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        $user->sendEmailVerificationNotification();
+        return $user;
     }
 }
